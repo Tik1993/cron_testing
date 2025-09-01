d094e5b6cc5891ca40abb6a0a1d5c5dbe6e73552
Tik1993 <81949896+Tik1993@users.noreply.github.com>
2025-08-31 18:12:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 
 def run_agent():
-    test1 = os.environ.get("TEST1", "not found")
+    test1 = os.environ.get("test1", "not found")
     print(test1)
     now = datetime.now()
     print(f"Running AI news agent at {now}")
